ab0f5782ba2e74d9df03a8dcc82f2d191e7e1051
Samuel Windall (Hull) <sam.smarty95@gmail.com>
2017-11-21 01:43:52 +1100

    Setup basic spring boot project

---
+++ src/main/kotlin/windall/console/account/api/Application.kt
@@ -0,0 +1,12 @@
+package windall.console.account.api
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
+}
